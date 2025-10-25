bdcf69ce88aa5f78579d7cce66010223d13452f5
endanistan <daniel.berg1991@outlook.com>
2025-10-25 22:02:35 +0200

    Enhance user creation functions and add Get-UserId
    Updated New-UserPassword and New-EntraUser functions to include additional adjectives and added Get-UserId function for user ID retrieval.

--- EntraTools/EntraTools.psm1
+++ EntraTools/EntraTools.psm1
@@ -1,5 +1,5 @@
 function New-UserPassword {
-    $Adjectives = @("Ferocious", "Sabertoothed", "Maneating", "Bloodthirsty", "Vengeful", "Merciless", "Wrathful", "Hellbound", "Soulharvesting", "Crazed", "Blessed", "Flesheating")
+    $Adjectives = @("Ferocious", "Sabertoothed", "Maneating", "Bloodthirsty", "Vengeful", "Merciless", "Wrathful", "Hellbound", "Soulharvesting", "Crazed", "Blessed", "Flesheating", "Purple", "Excited")
     $Nouns = @("Goldfish", "Froglet", "Bumblebee", "Pig", "Capybara", "Toad", "Rabbit", "Lamb", "Crab", "Shrimp", "Starfish")
     $Numbers = @("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
     $Characters = @("#", "%", "!", "?", "+", "\", "*", "$", "/")
@@ -10,6 +10,7 @@ function New-UserPassword {
     $RandomEasyToType = -join ($RandomAdjective, $RandomNoun, $RandomNumber, $RandomCharacter)
     return $RandomEasyToType
 }
+
 
 function New-EntraUser {
     param (
@@ -45,16 +46,37 @@ function New-EntraUser {
     return $NewUser
 }
 
+
+	function Get-UserId {
+        Param (
+            [Parameter(Mandatory = $true)][String]$UPN
+        )
+
+		Try {
+			$User = (Get-MgUser -Filter "UserPrincipalName eq '$UPN'").id
+			if (-not ($User)) {
+				throw
+			} else {
+                return $User
+			}
+		}
+		Catch {
+            return
+		}
+	}
+
+
 function Add-UserGroups {
     param (
         [Parameter(Mandatory = $true)][String]$UPN,
         [Parameter(Mandatory = $false)][String[]]$Groups,
         [switch]$CSV
     )
-
+		
     if ($CSV) {
         try {
-            $FilePath = Test-Path -Path ".\Groups.csv" -ErrorAction Stop
+            $FilePath = ".\Groups.csv"
+            Test-Path -Path $FilePath -ErrorAction Stop
         } catch {
             while ($true) {
                 $UserSpecifiedPath = Read-Host "Specify path to csv file: "
@@ -69,41 +91,60 @@ function Add-UserGroups {
         $Groups = Import-Csv -Path $FilePath -Delimiter ";" | Select-Object -ExpandProperty DisplayName
     }
 
-    foreach ($Group in $Groups) {
-        $AddToGroup = (Get-MgGroup -Filter "DisplayName eq '$Group'").id
-        $AddThisUser = (Get-MgUser -Filter "UserPrincipalName eq '$UPN'").id
-        if ($AddToGroup) {
-            try {
-                Get-MgGroupMemberByRef -GroupId $AddToGroup -DirectoryObjectId $AddThisUser -ErrorAction Stop | Out-Null
-                $Exists = $true
-            } catch {
-                $Exists = $false
-            }
-            if ($Exists) {
-                continue
-            }
-            Write-Output "Adding $UPN to $Group"
-            try {
-                New-MgGroupMember -GroupId $AddToGroup -DirectoryObjectId $AddThisUser -ErrorAction Stop
-            } catch {
-                Write-Output "$UPN could not be added to $Group"
-            }
+	$ValidatedGroups = @()
+	foreach ($Group in $Groups) {
+		Try {
+			$Validated = (Get-MgGroup -Filter "DisplayName eq '$Group'" -ErrorAction Stop).id
+			if ($Validated) {
+                Write-Output "Group $Group validated with object ID $Validated"
+				$ValidatedGroups += $Validated
+			} else {
+				throw
+			}
+		}
+        Catch {
+			Write-Warning "The group $Group does not exist, skipping..."
+		}
+	}
 
-            try {
-                if (Get-MgGroupMember -GroupId $AddToGroup -filter "Id eq '$AddThisUser'" -ErrorAction Stop) {
-					Write-Output "$UPN has been added to $Group"
-				} else {
-					Write-Output "$UPN has not been added to $Group"
-				}
-            } catch {
-                Write-Output "Failed to retrieve validation of output"
-            }
 
-        } else {
-            Write-Warning "The group $Group could not be found, skipping..."
-            continue
+    Try {
+        Write-Output "Fetching object ID for $UPN..."
+		$User = Get-UserId -UPN $UPN
+	}
+	Catch {
+		Write-Warning "Could not find $UPN in Entra ID, aborting"
+		Return
+	}
+
+
+	foreach ($ValidatedGroup in $ValidatedGroups) {
+		Try {
+			$MembersBefore = Get-MgGroupMember -GroupId $ValidatedGroup -All -ErrorAction SilentlyContinue
+			if (-not ($MembersBefore.Id -contains $User)) {
+				Write-Output "Attemping to add $UPN to $ValidatedGroup"
+				New-MgGroupMember -GroupId $ValidatedGroup -DirectoryObjectId $User -ErrorAction Stop
+			} else {
+				Write-Output "$UPN is already a member of $ValidatedGroup, skipping..."
+                Continue
+			}
         }
-    }
-}
+		Catch {
+            Write-Warning "Action could not be performed for $ValidatedGroup"
+			continue
+        }
 
+		Try {
+            $MembersAfter = Get-MgGroupMember -GroupId $ValidatedGroup -All -ErrorAction SilentlyContinue
+            if ($MembersAfter.Id -contains $User) {
+				Write-Output "$UPN has been added to $ValidatedGroup"
+			} else {
+				throw
+			}
+        }
+		Catch {
+            Write-Output "Failed to retrieve validation of output"
+        }
+	}
+}
 Export-ModuleMember -Function New-EntraUser, Add-UserGroups, New-UserPassword
