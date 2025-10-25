e533869bb2169dc4a8c07452b8da536fd3662f03
endanistan <daniel.berg1991@outlook.com>
2025-10-25 18:54:33 +0200

    First version of EntraTools
    Lightweight. Not particularly versatile but does some basic stuff.

---
+++ EntraTools/EntraTools.psd1
@@ -0,0 +1,36 @@
+@{
+    RootModule           = 'EntraTools.psm1'
+    ModuleVersion        = '1.0.0'
+    GUID                 = 'c997d099-f4b8-4747-8d47-ce15e1ab3851'
+
+    Author               = 'Daniel Berg'
+    Description          = 'Toolkit to create Entra ID users, generate OTP or add user group membership'
+
+    PowerShellVersion    = '7.0'
+
+    # Exporterade funktioner
+    FunctionsToExport    = @(
+        'New-UserPassword',
+        'New-EntraUser',
+        'Add-UserGroups'
+    )
+
+    CmdletsToExport      = @()
+    VariablesToExport    = @()
+    AliasesToExport      = @()
+
+    FileList             = @(
+        'EntraTools.psm1'
+    )
+
+    HelpInfoURI          = 'https://github.com/endanistan/MacGraphToolKits/EntraTools'
+    LicenseUri           = 'https://opensource.org/licenses/MIT'
+
+    PrivateData          = @{
+        PSData = @{
+            Tags         = @('Entra', 'AzureAD', 'Automation', 'UserManagement')
+            ProjectUri   = 'https://github.com/endanistan/MacGraphToolKits/EntraTools'
+            ReleaseNotes = 'Initial release with user creation, password generation, and group assignment.'
+        }
+    }
+}
---
+++ EntraTools/EntraTools.psm1
@@ -0,0 +1,106 @@
+﻿Export-ModuleMember -Function New-EntraUser, Add-UserGroups, New-UserPassword
+
+function New-UserPassword {
+    $Adjectives = @("Ferocious", "Sabertoothed", "Maneating", "Bloodthirsty", "Vengeful", "Merciless", "Wrathful", "Hellbound", "Soulharvesting", "Crazed", "Blessed", "Flesheating")
+    $Nouns = @("Goldfish", "Froglet", "Bumblebee", "Pig", "Capybara", "Toad", "Rabbit", "Lamb", "Crab", "Shrimp", "Starfish")
+    $Numbers = @("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+    $Characters = @("#", "%", "!", "?", "+", "\", "*", "$", "/")
+    $RandomAdjective = Get-Random $Adjectives
+    $RandomNoun = Get-Random $Nouns
+    $RandomNumber = Get-Random $Numbers
+    $RandomCharacter = Get-Random $Characters
+    $RandomEasyToType = -join ($RandomAdjective, $RandomNoun, $RandomNumber, $RandomCharacter)
+    return $RandomEasyToType
+}
+
+function New-EntraUser {
+    param (
+        [Parameter(Mandatory = $true)][String]$DN,
+        [Parameter(Mandatory = $true)][String]$UPN
+    )
+
+    $RandomPassword = New-UserPassword
+    $Passwordprofile = @{
+        Password                             = $RandomPassword
+        ForceChangePasswordNextSignIn        = $true
+        ForceChangePasswordNextSignInWithMfa = $false
+    }
+
+    $UserSplat = @{
+        DisplayName       = $DN
+        MailNickname      = ($UPN.Split('@')[0])
+        UserPrincipalName = $UPN
+        GivenName         = ($DN.Split(' ')[0])
+        Surname           = ($DN.Split(' ')[-1])
+        PasswordProfile   = $Passwordprofile
+        AccountEnabled    = $true
+    }
+
+
+    $NewUser = New-MgUser @UserSplat
+
+    if ((Get-MgUser -Filter "UserPrincipalName eq '$UPN'").Id) {
+        Write-Output "$DN added with username $UPN and password: $RandomPassword"
+    } else {
+        Write-Output "User $DN could not be added"
+    }
+    return $NewUser
+}
+
+function Add-UserGroups {
+    param (
+        [Parameter(Mandatory = $true)][String]$UPN,
+        [Parameter(Mandatory = $false)][String[]]$Groups,
+        [switch]$CSV
+    )
+
+    if ($CSV) {
+        try {
+            $FilePath = Test-Path -Path ".\Groups.csv" -ErrorAction Stop
+        } catch {
+            while ($true) {
+                $UserSpecifiedPath = Read-Host "Specify path to csv file: "
+                if (Test-Path -Path $UserSpecifiedPath) {
+                    $Groups = Import-Csv -Path $UserSpecifiedPath -Delimiter ";" | Select-Object -ExpandProperty DisplayName
+                    break
+                } else {
+                    Write-Warning "$UserSpecifiedPath not found. Try again, or press Ctrl C to exit the script"
+                }
+            }
+        }
+        $Groups = Import-Csv -Path $FilePath -Delimiter ";" | Select-Object -ExpandProperty DisplayName
+    }
+
+    foreach ($Group in $Groups) {
+        $AddToGroup = (Get-MgGroup -Filter "DisplayName eq '$Group'").id
+        $AddThisUser = (Get-MgUser -Filter "UserPrincipalName eq '$UPN'").id
+        if ($AddToGroup) {
+            try {
+                Get-MgGroupMemberByRef -GroupId $AddToGroup -DirectoryObjectId $AddThisUser -ErrorAction Stop | Out-Null
+                $Exists = $true
+            } catch {
+                $Exists = $false
+            }
+            if ($Exists) {
+                continue
+            }
+            Write-Output "Adding $UPN to $Group"
+            try {
+                New-MgGroupMember -GroupId $AddToGroup -DirectoryObjectId $AddThisUser -ErrorAction Stop
+            } catch {
+                Write-Output "$UPN could not be added to $Group"
+            }
+
+            try {
+                Get-MgGroupMemberByRef -GroupId $AddToGroup -DirectoryObjectId $AddThisUser -ErrorAction Stop | Out-Null
+                Write-Output "$UPN has been added to $Group"
+            } catch {
+                Write-Output "$UPN has not been added to $Group"
+            }
+
+        } else {
+            Write-Warning "The group $Group could not be found, skipping..."
+            continue
+        }
+    }
+}
